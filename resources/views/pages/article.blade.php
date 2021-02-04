e9a4523d50285d656d587998989703c6b26fdb17
Florence <monnaie.florence@gmail.com>
2021-02-04 18:21:43 +0100

    struct

---
+++ resources/views/pages/article.blade.php
@@ -0,0 +1,4 @@
+@extends('template.main')
+@section('content')
+    <h1>Page Article</h1>
+@endsection
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -8,7 +8,7 @@
       <nav class="nav-menu d-none d-lg-block">
         <ul>
           <li class="active"><a href="#">Home</a></li>
-          <li><a href="#">Article</a></li>
+          <li><a href="/articles">Articles</a></li>
           <li><a href="#">Contact</a></li>
         </ul>
       </nav><!-- .nav-menu -->
