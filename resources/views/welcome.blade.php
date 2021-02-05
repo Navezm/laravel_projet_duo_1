6a58293ce5dc3002f1f3a2157ddd4d1b1ce60392
Florence <monnaie.florence@gmail.com>
2021-02-05 10:05:17 +0100

    for martin's merge

--- resources/views/welcome.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('template.main')
-@section('content')
-<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsa sequi perferendis odio autem unde officia maxime veniam ab nemo. Ea quisquam maxime doloribus incidunt necessitatibus, ex voluptates voluptatibus in debitis, ducimus inventore quibusdam culpa tempore, ad suscipit! Laboriosam, fugiat. Inventore dicta, pariatur corrupti voluptatum quas provident neque earum quae ea dignissimos ducimus non commodi, veniam sunt est quod magni placeat aliquid totam hic, iure necessitatibus. Corrupti ea suscipit eius accusantium quia beatae. Molestiae, dolor repellat quae, deleniti eum vitae at quasi quidem id architecto impedit similique dolore aliquid. Saepe cum quidem ex id quibusdam unde ab nesciunt, laboriosam in iusto.</p>
-<h1 class="m-5">Page Home</h1>
-@endsection
